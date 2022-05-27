dbb001d00160ecd6c198ee971ec444ec686dae16
jlomako <71858760+jlomako@users.noreply.github.com>
2022-05-27 16:05:06 -0400

    Update hospital_tracker.R

--- hospital_tracker.R
+++ hospital_tracker.R
@@ -11,9 +11,8 @@ library(pdftools)
 # does does not work from github actions: server error 500
 # url <- "https://santemontreal.qc.ca/fileadmin/fichiers_portail/Donnees_urgence/urgence_quotidien_media.pdf"
 
-# does does not work from github actions: server error 500
 # little workaround: copy pdf to local directory and read from there 
-# download.file(url, "pdf/urgence_quotidien_media.pdf", mode="wb", headers = c("User-Agent" = "Chrome or something")) 
+# download.file(url, "pdf/urgence_quotidien_media.pdf", mode="wb", headers = c("User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36")) 
 
 # use local copy
 url <- "pdf/urgence_quotidien_media.pdf"
