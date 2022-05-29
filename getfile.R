44897de1675eed8c3e6888d3afb36a4ee0ce2b37
jlomako <71858760+jlomako@users.noreply.github.com>
2022-05-29 10:51:21 -0400

    Update getfile.R

--- getfile.R
+++ getfile.R
@@ -1,4 +1,5 @@
 # get pdf and save to depository
 
-url <- "https://santemontreal.qc.ca/fileadmin/fichiers_portail/Donnees_urgence/urgence_quotidien_media.pdf"
-download.file(url, "pdf/urgence_quotidien_media.pdf", mode="wb", headers = c("User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36")) 
+url <- "https://www.msss.gouv.qc.ca/professionnels/statistiques/documents/urgences/Rap_horaire_SituatUrgence1.pdf"
+download.file(url, "pdf/urgence_quotidien_media.pdf", mode="wb")) 
+# download.file(url, "pdf/urgence_quotidien_media.pdf", mode="wb", headers = c("User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36")) 
